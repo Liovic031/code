e15875bfb8cee639b6c7097b73234d4fb9370170
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2022-05-19 16:33:39 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "funkcije.h"
+#include "struktura.h"
+
+
+int main(void) {
+
+	return 0;
+}
+
---
+++ struktura.h
@@ -0,0 +1,10 @@
+
+#ifndef STRUKTURA_H
+#define STRUKTURA_H
+
+struct player {
+	char ime[20];
+	int bodovi;
+}PLAYER;
+
+#endif
